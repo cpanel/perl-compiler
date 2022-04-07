ebfa1a6ebcd31433a43655fc9ea1949a2f58691d
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2022-04-07 21:38:10 +0000

    Use function signature in B::REGEXP

--- lib/B/C/OverLoad/B/REGEXP.pm
+++ lib/B/C/OverLoad/B/REGEXP.pm
@@ -1,6 +1,6 @@
 package B::REGEXP;
 
-use strict;
+use B::C::Std;
 
 use B qw/RXf_EVAL_SEEN/;
 use B::C::Debug qw/debug/;
@@ -8,8 +8,7 @@ use B::C::File qw/init1 init2 svsect xpvsect/;
 use B::C::Save qw/savecowpv/;
 
 # post 5.11: When called from B::RV::save not from PMOP::save precomp
-sub do_save {
-    my ( $sv, $fullname ) = @_;
+sub do_save( $sv, $fullname=undef ) {
 
     $sv->FLAGS & 2048 and die sprintf( "Unexpected SVf_ROK found in %s\n", ref $sv );
 
