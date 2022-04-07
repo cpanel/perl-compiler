24556b302b4c8be53ff1e1ea1a5415e67b03d37c
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2022-04-07 21:10:49 +0000

    Use function signature in B::IV

--- lib/B/C/OverLoad/B/IV.pm
+++ lib/B/C/OverLoad/B/IV.pm
@@ -1,13 +1,12 @@
 package B::IV;
 
-use strict;
+use B::C::Std;
 
 use B qw/SVf_IVisUV/;
 use B::C::File qw/svsect/;
 use B::C::Decimal qw/get_integer_value/;
 
-sub do_save {
-    my ( $sv, $fullname, $custom ) = @_;
+sub do_save( $sv, $fullname=undef, $custom=undef ) {
 
     my $rv_sym = B::RV::try_save( $sv, $fullname );
     return $rv_sym if $rv_sym;
