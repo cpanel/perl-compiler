a5792fb351edbb84ccd4d5852addd71b83421af3
Todd Rinaldo <toddr@cpan.org>
2022-07-22 15:41:22 +0000

    Update Std.pm for 5.36 feature bundle

--- lib/B/C/Std.pm
+++ lib/B/C/Std.pm
@@ -4,13 +4,10 @@ use strict;
 
 sub import {
 
-    strict->import();
-
     # comp/use.t is blocking feature.pm... we can safely load it from this point
     delete $INC{'feature.pm'} if defined $INC{'feature.pm'} && $INC{'feature.pm'} !~ m{/} && !-e $INC{'feature.pm'};
     require feature;
-    feature->import( ':5.36', 'signatures' );
-    warnings->unimport('experimental::signatures');
+    feature->import( ':5.36' );
 
     return;
 }
